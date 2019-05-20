768db91da407b965cd4cfd2c54795d4b610a7661
Lê Hoài Nam <48401609+hoainam1999@users.noreply.github.com>
2019-05-20 23:32:45 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,5 +1,5 @@
 class py_solution():
     def reverse_words(self, s):
-        return ''.join(reversed(s.split()))
+        return ''.join(reversed(s))
 
 print(py_solution().reverse_words('Hello.py'))
